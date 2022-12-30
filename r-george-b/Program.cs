e24749912b2eb2e096448ed32d676bcdfb10aa6a
georgeseeger <georgeseeger@hotmail.com>
2022-12-30 17:15:03 +0000

    Try to init a few times in case OpenRGB isn't on yet

--- r-george-b/Program.cs
+++ r-george-b/Program.cs
@@ -1,30 +1,43 @@
-﻿using OpenRGB.NET;
+using OpenRGB.NET;
 using OpenRGB.NET.Models;
+using  System.Linq;
 
-await Task.Delay(3000);
-using var client = new OpenRGBClient(name: "My OpenRGB Client", autoconnect: true, timeout: 1000);
+for (var i = 4; i >= 0; i--) {
+    try {
+        await ManageLedsAsync();
+    }
+    catch {
+        if (i == 0) throw;
+        await Task.Delay(10_000);
+    }
+}
 
 
-var deviceCount = client.GetControllerCount();
-var devices = client.GetAllControllerData();
-var cycleTimeMs = args.Any() && int.TryParse(args[0], out var cycle) ? cycle : 16_000;
-const int updateTimeMs = 100;
+async Task ManageLedsAsync() {
+    using var client = new OpenRGBClient(name: "My OpenRGB Client", autoconnect: true, timeout: 1000);
+
+
+    var deviceCount = client.GetControllerCount();
+    var devices = client.GetAllControllerData();
+    var cycleTimeMs = args.Any() && int.TryParse(args[0], out var cycle) ? cycle : 16_000;
+    const int updateTimeMs = 100;
 
 
 var rainbow = Color.GetHueRainbow(cycleTimeMs / updateTimeMs).ToArray();
-var c = 0;
+    var c = 0;
 
-while (true) {
+    while (true) {
     c = (c + 1) % rainbow.Length;
     var nextColour = rainbow[c];
 
-    for (int i = 0; i < devices.Length; i++) {
-        var device = devices[i];
-        var leds = Enumerable.Range(0, device.Colors.Length)
-            .Select(_ => nextColour)
-            .ToArray();
-        client.UpdateLeds(i, leds);
-    }
+        for (int i = 0; i < devices.Length; i++) {
+            var device = devices[i];
+            var leds = Enumerable.Range(0, device.Colors.Length)
+                .Select(_ => nextColour)
+                .ToArray();
+            client.UpdateLeds(i, leds);
+        }
 
-    await Task.Delay(updateTimeMs);
+        await Task.Delay(updateTimeMs);
+    }
 }
