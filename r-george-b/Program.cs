e7952516dca8e718e864a4dcc65a36856a795ea5
georgeseeger <georgeseeger@hotmail.com>
2023-01-09 18:57:55 +0000

    Fix keyboard LEDs because key indexes are not the same every time

--- r-george-b/Program.cs
+++ r-george-b/Program.cs
@@ -3,77 +3,97 @@ using OpenRGB.NET.Models;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-for (var i = 4; i >= 0; i--) {
-    try {
-        await ManageLedsAsync();
-    }
-    catch {
-        if (i == 0) throw;
-        await Task.Delay(10_000);
-    }
-}
-
-
-async Task ManageLedsAsync() {
-    using var client = new OpenRGBClient(name: "My OpenRGB Client", autoconnect: true, timeout: 1000);
-
-    var deviceCount = client.GetControllerCount();
-    var devices = client.GetAllControllerData();
-    var keyboardDefault = KeyboardConfig();
-    
-    var cycleTimeMs = args.Any() && int.TryParse(args[0], out var cycle) ? cycle : 16_000;
-    const int updateTimeMs = 500;
-
-    var spectrum = Color.GetHueRainbow(cycleTimeMs / updateTimeMs).ToArray();
-    // var spectrum = Color.GetSinRainbow(cycleTimeMs / updateTimeMs, 130, 125, 1, 0).ToArray();
-    var c = 0;
-
-    while (true) {
-        c = (c + 1) % spectrum.Length;
-        var nextColour = spectrum[c];
-
-        var gpuDevice = devices.IndexOf(isGpu);
-        var directMode = 0;
-        if (devices.Any(isGpu)) {
-            directMode = ((Mode[])devices[gpuDevice].Modes).IndexOf(m => m.Name == "Direct");
-        }
-
-        for (int i = 0; i < devices.Length; i++) {
-            var device = devices[i];
-            var leds = Enumerable.Range(0, device.Colors.Length)
-                .Select(_ => nextColour)
-                .ToArray();
-            if (isGpu(device)) {
-                client.UpdateLeds(i, leds);
-                client.SetMode(i, directMode);
-                // client.SaveMode(i);
+public class Program {
+    public static async Task Main(string[] args) {
+        for (var i = 4; i >= 0; i--) {
+            try {
+                await ManageLedsAsync();
             }
-            else if (isKeyboard(device)) {
-                var keyboardLeds = keyboardDefault.Select(c => c.Equals(new Color(0, 255, 0)) ? nextColour : c).ToArray();
-                client.UpdateLeds(i, keyboardLeds);
-            } else
-                client.UpdateLeds(i, leds);
+            catch {
+                if (i == 0) throw;
+                await Task.Delay(10_000);
+            }
         }
 
-        await Task.Delay(updateTimeMs);
+        async Task ManageLedsAsync() {
+            using var client = new OpenRGBClient(name: "My OpenRGB Client", autoconnect: true, timeout: 1000);
+
+            var deviceCount = client.GetControllerCount();
+            var devices = client.GetAllControllerData();
+            
+            var cycleTimeMs = args.Any() && int.TryParse(args[0], out var cycle) ? cycle : 16_000;
+            const int updateTimeMs = 500;
+
+            var spectrum = Color.GetHueRainbow(cycleTimeMs / updateTimeMs).ToArray();
+            // var spectrum = Color.GetSinRainbow(cycleTimeMs / updateTimeMs, 130, 125, 1, 0).ToArray();
+            var c = 0;
+
+            while (true) {
+                c = (c + 1) % spectrum.Length;
+                var nextColour = spectrum[c];
+
+                var gpuDevice = devices.IndexOf(isGpu);
+                var directMode = 0;
+                if (devices.Any(isGpu)) {
+                    directMode = ((Mode[])devices[gpuDevice].Modes).IndexOf(m => m.Name == "Direct");
+                }
+
+                Func<int, bool> shouldUpdateLedOnKeyboard = _ => true;
+                if (devices.Any(isKeyboard)) {
+                    shouldUpdateLedOnKeyboard = ShouldUpdateLedOnKeyboard(devices.First(isKeyboard));
+                }
+
+                for (int i = 0; i < devices.Length; i++) {
+                    var device = devices[i];
+                    var leds = Enumerable.Range(0, device.Colors.Length)
+                        .Select(_ => nextColour)
+                        .ToArray();
+                    if (isGpu(device)) {
+                        client.UpdateLeds(i, leds);
+                        client.SetMode(i, directMode);
+                        // client.SaveMode(i);
+                    }
+                    else if (isKeyboard(device)) {
+                        var keyboardLeds = device.Colors.Select((c, i) => shouldUpdateLedOnKeyboard(i) ? nextColour : c).ToArray();
+                        client.UpdateLeds(i, keyboardLeds);
+                    } else
+                        client.UpdateLeds(i, leds);
+                }
+
+                await Task.Delay(updateTimeMs);
+            }
+
+            bool isGpu(Device device) => device.Type == OpenRGB.NET.Enums.DeviceType.Gpu;
+
+            bool isKeyboard(Device device) => device.Type == OpenRGB.NET.Enums.DeviceType.Keyboard;
+
+            Func<int, bool> ShouldUpdateLedOnKeyboard(Device keyboard) {
+                var keyIndexes = keyboard.Leds.Select((key, i) => (key, i))
+                                .Where(t => t.key.Name.Contains("Number Pad")
+                                    || t.key.Name.Contains("Arrow")
+                                    || t.key.Name.Contains("Media")
+                                    || t.key.Name.Contains("Logo")
+                                    || NamedKeys.Contains(t.key.Name))
+                                .Select(t => t.i)
+                                .ToHashSet();
+                return keyIndexes.Contains;
+            } 
+        }
     }
+
+    private static HashSet<string> NamedKeys = new HashSet<string> {
+        "Key: Print Screen",
+        "Key: Scroll Lock",
+        "Key: Num Lock",
+        "Key: Pause/Break",
+        "Key: Insert",
+        "Key: Delete",
+        "Key: Home",
+        "Key: End",
+        "Key: Page Up",
+        "Key: Page Down",
+    };
 }
-
-bool isGpu(Device device) => device.Type == OpenRGB.NET.Enums.DeviceType.Gpu;
-
-bool isKeyboard(Device device) => device.Type == OpenRGB.NET.Enums.DeviceType.Keyboard;
-
-Color[] KeyboardConfig() {
-    var keyboardColourString = @"R:255,G:0,B:0},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:255,G:255,B:0},{R:0,G:12,B:255},{R:255,G:255,B:0},{R:0,G:255,B:0},{R:255,G:255,B:255},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:0,B:0},{R:0,G:12,B:255},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:255,G:255,B:255},{R:255,G:145,B:0},{R:255,G:0,B:0},{R:255,G:0,B:0},{R:255,G:145,B:0},{R:0,G:12,B:255},{R:0,G:255,B:0},{R:0,G:12,B:255},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:255,G:255,B:255},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:0,B:0},{R:255,G:145,B:0},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:255,G:255,B:255},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:0,G:12,B:255},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:255,G:255,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:255,G:255,B:255},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:0,G:255,B:0},{R:0,G:12,B:255},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:255,G:255,B:255},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:255,G:255,B:255},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:255,G:255,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:255,G:255,B:255},{R:255,G:145,B:0},{R:255,G:145,B:0},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:255,G:255,B:255},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:255,G:255,B:255},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:0,G:12,B:255},{R:0,G:12,B:255},{R:0,G:255,B:0},{R:0,G:255,B:0},{R:255,G:0,B:0";
-    return keyboardColourString
-        .Split("},{")
-        .Select(s => Regex.Match(s, @"R:(\d+),G:(\d+),B:(\d+)"))
-        .Select(m =>
-        {
-            return new Color(byte.Parse(m.Groups[1].Value), byte.Parse(m.Groups[2].Value), byte.Parse(m.Groups[3].Value));
-        })
-        .ToArray();
-} 
 
 public static class EnumerableExtensions {
     public static int IndexOf<T>(this T[] array, Func<T, bool> predicate) {
